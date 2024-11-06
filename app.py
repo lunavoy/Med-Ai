d025e54e38832758afd1f69c0b5dec8503d3b65b
lunavoy <luisacarreiroclaudino@gmail.com>
2024-11-06 18:52:16 -0300

    Appo.appoin_datetime >= datetime.date.today()

--- app.py
+++ app.py
@@ -12,16 +12,16 @@ import datetime
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SECRET_KEY'] = 'thisisasecretkey'
-db = SQLAlchemy(app) # Inicializando a base de dados
+db = SQLAlchemy(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
-login_manager.login_view = 'login'  # Rota onde será redirecionado se o login for necessário
+login_manager.login_view = 'login'
 migrate = Migrate(app, db)
 #-----------------------------------Tabelas--------------------------------------------------
-class User(db.Model, UserMixin):
+class Patient(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), nullable=False, unique=True)
-    name = db.Column(db.String(50), nullable=False) 
+    name = db.Column(db.String(50), nullable=False)
     password = db.Column(db.String(12), nullable=False)
 
 class Doctor(db.Model, UserMixin):
@@ -32,116 +32,90 @@ class Doctor(db.Model, UserMixin):
     specialty = db.Column(db.String(30), nullable=False)
     crm = db.Column(db.String(6), nullable=False)
     estado = db.Column(db.String(30), nullable=False, default="")
-    pfp = db.Column(db.String(100), nullable=True)
 
-    def verify_password(self, password):
-        return check_password_hash(self.password_hash, password)
-    
 class Appointment(db.Model):
-    __tablename__ = 'appointment'
-    
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    patient_id = db.Column(db.Integer, db.ForeignKey('patient.id'), nullable=False)
     doctor_id = db.Column(db.Integer, db.ForeignKey('doctor.id'), nullable=False)
-    appointment_date = db.Column(db.Date, nullable=False)
+    appointment_datetime = db.Column(db.DateTime, nullable=False)
 
-    user = db.relationship('User', backref='appointments')
+    patient = db.relationship('Patient', backref='appointments')
     doctor = db.relationship('Doctor', backref='appointments')
 
-    __table_args__ = (db.UniqueConstraint('doctor_id', 'appointment_date', name='_appointment_date_doctor_id_uc'),)
+    __table_args__ = (db.UniqueConstraint('doctor_id', 'appointment_datetime', name='_appointment_datetime_doctor_id_uc'),)
 
 class DoctorRating(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     doctor_id = db.Column(db.Integer, db.ForeignKey('doctor.id'), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    patient_id = db.Column(db.Integer, db.ForeignKey('patient.id'), nullable=False)
     rating = db.Column(db.Integer, nullable=False)
     review = db.Column(db.String(500), nullable=True)
-
+    
 with app.app_context():
    db.create_all()
     #db.create_all() # Doctor.__table__.drop(db.engine)
+
+from datetime import datetime
+
+# Add the new appointment dates and times to the database
+
 #------------------------------------------------------------------------------ROTAS(URL)-----------------------------------------------------------------------
+@login_manager.user_loader
+def load_user(user_id):
+    if session.get('user_type') == 'patient':
+        return Patient.query.get(int(user_id))
+    elif session.get('user_type') == 'doctor':
+        return Doctor.query.get(int(user_id))
+    return None
 @app.route('/') #Página Inicial
 def home():
     return render_template('home.html')
-@login_manager.user_loader # Função que carrega o usuário baseado no ID
-def load_user(user_id):
-    user = User.query.get(int(user_id))
-    if user is not None:
-        return user
-
-    doctor = Doctor.query.get(int(user_id))
-    if doctor is not None:
-        return doctor  
-    return None
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        
-        user = User.query.filter_by(username=username).first()
-        if user and check_password_hash(user.password, password):
-            login_user(user)
-            session['user_type'] = 'patient'
-            return redirect(url_for('menu'))
-        
-        doctor = Doctor.query.filter_by(username=username).first()
-        if doctor and check_password_hash(doctor.password, password):
-            login_user(doctor)
-            session['user_type'] = 'doctor'
-            return redirect(url_for('dashboard_doctor'))
-        else:
-            flash("Username ou senha incorretos.")
-
-    if session.get('user_type') == 'patient':
-        print("Logged-in user is a patient")
-    elif session.get('user_type') == 'doctor':
-        print("Logged-in user is a doctor")
-    return render_template('login.html')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
-        username = request.form.get('username')
-        name = request.form.get('name')
-        password = request.form.get('password')
-
-        if not username or not name or not password:
-            flash("Todos os campos são obrigatórios.")
-            return redirect(url_for('register'))
-
-        # Verificação de existência de username em ambas as tabelas
-        existing_user = User.query.filter_by(username=username).first()
-        existing_doctor = Doctor.query.filter_by(username=username).first()
+        user_type = request.form.get('user_type')
         
-        if existing_user or existing_doctor:
-            flash('O nome de usuário já está em uso. Tente outro.')
-            return redirect(url_for('register'))
-
-        user_type = request.form.get('user_type')  # Verifica se é paciente ou médico
-
+        # Patient Registration
         if user_type == 'patient':
+            username = request.form.get('username')
+            name = request.form.get('name')
+            password = request.form.get('password')
+
+            if not username or not name or not password:
+                flash("Todos os campos são obrigatórios.")
+                return redirect(url_for('register'))
+
+            existing_user = Patient.query.filter_by(username=username).first()
+            if existing_user:
+                flash('O nome de usuário já está em uso. Tente outro.')
+                return redirect(url_for('register'))
+
             hashed_password = generate_password_hash(password)
-            new_user = User(username=username, name=name, password=hashed_password)
+            new_user = Patient(username=username, name=name, password=hashed_password)
             db.session.add(new_user)
             db.session.commit()
             flash('Cadastro de paciente realizado com sucesso!')
             return redirect(url_for('login'))
 
+        # Doctor Registration
         elif user_type == 'doctor':
+            username = request.form.get('username')
+            name = request.form.get('name')
+            password = request.form.get('password')
             specialty = request.form.get('specialty')
             crm = request.form.get('crm')
-            estado = request.form.get('estado')  # Novo campo para o estado do CRM
+            estado = request.form.get('estado')
 
-            if not specialty or not crm or not estado:
+            if not username or not name or not password or not specialty or not crm or not estado:
                 flash("Todos os campos de médico são obrigatórios.")
                 return redirect(url_for('register'))
 
-            # Verifica se já existe um médico com o mesmo CRM e estado
+            existing_doctor = Doctor.query.filter_by(username=username).first()
             existing_doctor_crm = Doctor.query.filter_by(crm=crm, estado=estado).first()
-            if existing_doctor_crm:
-                flash('Já existe um médico com este CRM e estado. Faça login ou tente novamente.')
+            
+            if existing_doctor or existing_doctor_crm:
+                flash('O nome de usuário ou CRM já está em uso. Tente outro.')
                 return redirect(url_for('register'))
 
             hashed_password = generate_password_hash(password)
@@ -152,39 +126,66 @@ def register():
             return redirect(url_for('login'))
 
     return render_template('register.html')
+    
+    
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        patient = Patient.query.filter_by(username=username).first()
+        doctor = Doctor.query.filter_by(username=username).first()
+
+        if patient and check_password_hash(patient.password, password):
+            login_user(patient)
+            session['user_type'] = 'patient'
+            flash("Login realizado com sucesso.")
+            return redirect(url_for('menu'))
+        elif doctor and check_password_hash(doctor.password, password):
+            login_user(doctor)
+            session['user_type'] = 'doctor'
+            flash("Login realizado com sucesso.")
+            return redirect(url_for('dashboard_doctor'))
+        else:
+            flash("Username ou senha incorretos.")
+            return redirect(url_for('login'))
+
+    return render_template('login.html')
 
 @app.route('/logout')
-@login_required
 def logout():
     logout_user()
+    session.pop('user_type', None)
     flash('Logout realizado com sucesso.')
     return redirect(url_for('login'))
-
 @app.route('/menu')
 @login_required
 def menu():
-    # Verifica se o usuário atual é um médico
-    if isinstance(current_user, Doctor):
-        flash("Acesso restrito a pacientes.")
-        return redirect(url_for('dashboard_doctor'))
-    
-    # Caso seja um paciente, renderiza o menu
-    return render_template('menu.html')
-        
+    if session.get('user_type') != 'patient':
+        abort(403)  # Restrict access if not a patient
+    upcoming_appointments = Appointment.query.filter(
+        Appointment.patient_id == current_user.id,
+        Appointment.appointment_datetime >= datetime.date.today()
+    ).order_by(Appointment.appointment_datetime).all()
+    return render_template('menu.html', upcoming_appointments=upcoming_appointments)
+
 @app.route('/dashboard_doctor', endpoint='dashboard_doctor')
 @login_required
 def dashboard_doctor():
-    if not isinstance(current_user, Doctor):
-        abort(403)
-
     upcoming_appointments = Appointment.query.filter(
         Appointment.doctor_id == current_user.id,
-        Appointment.appointment_date >= datetime.date.today()
-    ).order_by(Appointment.appointment_date).all()
+        Appointment.appointment_datetime >= datetime.date.today()
+    ).order_by(Appointment.appointment_datetime).all()
 
     ratings = DoctorRating.query.filter_by(doctor_id=current_user.id).all()
     average_rating = sum(r.rating for r in ratings) / len(ratings) if ratings else None
     latest_reviews = DoctorRating.query.filter_by(doctor_id=current_user.id).order_by(DoctorRating.id.desc()).limit(5).all()
+
+    if 'logout' in request.args:
+        logout_user()
+        flash('Logout realizado com sucesso.')
+        return redirect(url_for('login'))
 
     return render_template(
         'dashboard_doctor.html',
@@ -212,59 +213,63 @@ def select_doctor():
     
     # Renderiza a página de seleção de especialidades sem médicos selecionados inicialmente
     return render_template('select_doctor.html', specialties=specialties)
-
 @app.route('/schedule_appointment/<int:doctor_id>', methods=['GET', 'POST'])
 @login_required  # O paciente precisa estar logado
 def schedule_appointment(doctor_id):
     doctor = Doctor.query.get_or_404(doctor_id)  # Busca o médico pelo ID
     
     if request.method == 'POST':
-        # Pega e valida a data do formulário de agendamento
-        appointment_date_str = request.form.get('appointment_date')
+        appointment_date_str = request.form.get('appointment_date') 
+        appointment_time_str = request.form.get('appointment_time')
+        appointment_datetime_str = f'{appointment_date_str} {appointment_time_str}' # Pega e valida a data do formulário de agendamento
+        
+        appointment_datetime_str = appointment_datetime_str.replace('None ', '')  # Tira o 'None' do come o da string
         try:
-            appointment_date = datetime.datetime.strptime(appointment_date_str, '%Y-%m-%d').date()
-            
-            # Verifica se a data não está no passado
-            if appointment_date < datetime.date.today():
-                flash('A data selecionada é no passado. Escolha uma data futura.', 'error')
-                return redirect(url_for('schedule_appointment', doctor_id=doctor_id))
-                
+            appointment_datetime = datetime.datetime.strptime(appointment_datetime_str, '%Y-%m-%d %H:%M')
         except ValueError:
-            flash('Data inválida. Por favor, use o formato AAAA-MM-DD.', 'error')
+            flash('Formato de data ou hora inválido. Use o formato aaaa-mm-dd HH:MM.', 'error')
             return redirect(url_for('schedule_appointment', doctor_id=doctor_id))
 
-        # Verifica se já existe um agendamento para esse médico na data selecionada
-        existing_appointment = Appointment.query.filter_by(doctor_id=doctor_id, appointment_date=appointment_date).first()
-        if existing_appointment:
-            flash('Esse médico já tem uma consulta agendada nessa data. Escolha outra data.', 'error')
+        if appointment_datetime < datetime.datetime.now():  # data e hora no passado = error
+            flash('A data e hora selecionadas são no passado. Escolha uma data futura.', 'error')
             return redirect(url_for('schedule_appointment', doctor_id=doctor_id))
+
+        # Verifica se j  existe um agendamento para esse m dico na data e hora selecionados
+        booked_time_slots = Appointment.query.filter(
+            Appointment.doctor_id == doctor_id,
+            db.func.date(Appointment.appointment_datetime) == appointment_datetime.date()
+        ).all()
+        for appointment in booked_time_slots:
+            if appointment.appointment_datetime == appointment_datetime:
+                flash('Esse médico já tem uma consulta agendada nessa data e hora. Escolha outra data ou hora.', 'error')
+                return redirect(url_for('schedule_appointment', doctor_id=doctor_id))
         
         # Cria uma nova consulta
         new_appointment = Appointment(
-            user_id=current_user.id,  # Paciente logado
+            patient_id=current_user.id,  # Paciente logado
             doctor_id=doctor_id,
-            appointment_date=appointment_date
+            appointment_datetime=appointment_datetime
         )
         db.session.add(new_appointment)
         db.session.commit()
         
-        flash(f'Consulta agendada com o Dr. {doctor.name} para o dia {appointment_date}.', 'success')
+        flash(f'Consulta agendada com o Dr. {doctor.name} para o dia e hora {appointment_datetime}.', 'success')
         return redirect(url_for('menu'))  # Redireciona após o agendamento
 
-    return render_template('schedule_appointment.html', doctor=doctor)
-
+    available_times = [f"{hour}:00" for hour in range(8, 19)]  # Horários disponíveis das 8 às 18
+    return render_template('schedule_appointment.html', doctor=doctor, available_times=available_times)
 @app.route('/select-doctor-for-rating')
 @login_required
 def select_doctor_for_rating():
-    user_id = current_user.id
+    patient_id = current_user.id
     today = datetime.date.today()
 
     # Consultas anteriores ou na data atual, onde ainda não há avaliação
     appointments = (
         Appointment.query
         .filter(
-            Appointment.user_id == user_id,
-            Appointment.appointment_date <= today
+            Appointment.patient_id == patient_id,
+            Appointment.appointment_datetime <= today
         )
         .outerjoin(DoctorRating, DoctorRating.doctor_id == Appointment.doctor_id)
         .filter(DoctorRating.id.is_(None))  # Exclui consultas já avaliadas
@@ -276,7 +281,7 @@ def select_doctor_for_rating():
 @login_required
 def rate_doctor(doctor_id):
     doctor = Doctor.query.get_or_404(doctor_id)  # Busca o médico pelo ID
-    user_id = current_user.id  # ID do usuário logado
+    patient_id = current_user.id  # ID do paciente logado
     
     if request.method == 'POST':
         # Valida e converte a nota
@@ -293,21 +298,21 @@ def rate_doctor(doctor_id):
         
         # Verifica se existe uma consulta passada ou atual sem avaliação
         appointment = Appointment.query.filter(
-            Appointment.user_id == user_id,
+            Appointment.patient_id == patient_id,
             Appointment.doctor_id == doctor_id,
-            Appointment.appointment_date <= datetime.date.today()
+            Appointment.appointment_datetime <= datetime.date.today()
         ).first()
 
         existing_rating = DoctorRating.query.filter_by(
             doctor_id=doctor_id, 
-            user_id=user_id
+            patient_id=patient_id
         ).first() if appointment else None
 
         if appointment and not existing_rating:
             # Cria a avaliação
             new_rating = DoctorRating(
                 doctor_id=doctor_id,
-                user_id=user_id,
+                patient_id=patient_id,
                 rating=rating,
                 review=review
             )
@@ -319,23 +324,6 @@ def rate_doctor(doctor_id):
             flash('Você não pode avaliar este médico ou a consulta já foi avaliada.', 'danger')
 
     return render_template('rate_doctor.html', doctor=doctor)
-@app.route('/upload_profile_image', methods=['POST'])
-@login_required
-def upload_profile_image():
-    if not isinstance(current_user, Doctor):
-        return redirect(url_for('menu'))
-
-    if 'profile_image' in request.files:
-        file = request.files['profile_image']
-        if file.filename != '':
-            filename = secure_filename(file.filename)
-            filepath = os.path.join('static/profile_images', filename)
-            file.save(filepath)
-            current_user.profile_image = filename
-            db.session.commit()
-            flash("Imagem de perfil atualizada com sucesso.")
-    
-    return redirect(url_for('dashboard_doctor'))
 
 @app.route('/check-data', methods=['GET'])
 def check_data():
@@ -352,6 +340,4 @@ def check_data():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
 
